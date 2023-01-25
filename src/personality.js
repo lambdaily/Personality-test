8eb6651c175d4400dafbb0ceda0e254b9423ecdc
lambdaily <pablo.espinoza.mail@gmail.com>
2023-01-25 17:35:44 -0300

    cambio de imagenes de muestra

--- src/personality.js
+++ src/personality.js
@@ -2,7 +2,7 @@ export const personality = [
   {
     id: 1,
     imgUrl:
-      'https://static.wikia.nocookie.net/rikolino/images/8/81/Calamardo_Hermoso.png/revision/latest?cb=20150511012719&path-prefix=es',
+      'https://w7.pngwing.com/pngs/80/571/png-transparent-happiness-husband-hug-love-woman-couple-household-people-couple.png',
     title: 'Novio 1',
     details: 'Eres el novio de tipo 1',
   },
@@ -10,14 +10,14 @@ export const personality = [
     id: 2,
     title: 'Novio 2',
     imgUrl:
-      'https://i.pinimg.com/originals/cb/33/d8/cb33d83d195d9a53037ba9ef4205dc0f.png',
+      'https://www.pngfind.com/pngs/m/346-3467580_pareja-de-enamorados-png-happy-couple-png-transparent.png',
     details: 'Eres el novio de tipo 2',
   },
   {
     id: 3,
     title: 'Novio 3',
     imgUrl:
-      'https://static.wikia.nocookie.net/rikolino/images/8/81/Calamardo_Hermoso.png/revision/latest?cb=20150511012719&path-prefix=es',
+      'https://www.pngitem.com/pimgs/m/350-3509098_terapia-de-pareja-parejas-de-modelo-png-transparent.png',
     details: 'Eres el novio de tipo 3',
   },
 ];
