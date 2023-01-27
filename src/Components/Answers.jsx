7bfc5d2f1b832b660094d626a14bb28103ead8a4
lambdaily <pablo.espinoza.mail@gmail.com>
2023-01-27 09:50:19 -0300

    Logica para que funcione componente Answers

--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -4,13 +4,34 @@ const Answers = ({ topScore }) => {
   return (
     <>
       {personality.map((element, index) => {
-        return (
-          <div key={index}>
-            <h2 key={index}>{element.title}</h2>
-            <p>{element.details}</p>
-            <img src={element.imgUrl} alt="" />
-          </div>
-        );
+        if ((topScore / 3 < 2) & (index == 0)) {
+          return (
+            <div key={index}>
+              <h2>{element.title}</h2>
+              <p>{element.details}</p>
+              <img src={element.imgUrl} alt="" />
+            </div>
+          );
+        }
+
+        if ((topScore / 3 == 2) & (index == 1)) {
+          return (
+            <div key={index}>
+              <h2>{element.title}</h2>
+              <p>{element.details}</p>
+              <img src={element.imgUrl} alt="" />
+            </div>
+          );
+        }
+        if ((topScore / 3 > 2) & (index == 2)) {
+          return (
+            <div key={index}>
+              <h2>{element.title}</h2>
+              <p>{element.details}</p>
+              <img src={element.imgUrl} alt="" />
+            </div>
+          );
+        }
       })}
     </>
   );
