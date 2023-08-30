3800849d7ea69d43c0a307523034cf0fff820ec0
Pablo Espinoza <astragant97@yahoo.com>
2023-08-30 13:54:28 -0400

    Update personality.js

--- src/personality.js
+++ src/personality.js
@@ -1,24 +1,24 @@
 export const personality = [
   {
     id: 1,
-    imgUrl: 'https://media.tenor.com/PBm8Xocb1VEAAAAd/chopper-tony-chopper.gif',
-    title: 'Te falta romanticismo 🤨🏳️‍🌈?',
+    imgUrl: 'https://media2.giphy.com/media/phFp1p0uN66WksDHks/giphy.gif?cid=ecf05e47y4r0kkzxwzilxvf6fiqnoxljwpt9plh5eobsfvc4&ep=v1_gifs_search&rid=giphy.gif&ct=g',
+    title: 'Poco sentimental',
     details:
-      'Aflojar el corazon y la billetera de vez en cuando no te haría mal, ',
+      'No muestras una gran cantidad de sentimientos románticos o emocionales hacia tu pareja, quizas el ser un poco más detallista mejore tu relación',
   },
   {
     id: 2,
-    title: 'Guapo/a 😳',
+    title: 'Compañero/a😳',
     imgUrl:
       'https://media3.giphy.com/media/XreQmk7ETCak0/giphy.gif?cid=790b7611c22629dd0d0eb47fb0a58b57f6e32b15ead555f1&rid=giphy.gif&ct=g',
     details:
-      'Eres un buen novio/a causarás mas de un suspiro de vez en cuando ',
+      'Eres una persona que reconforta a su pareja en los buenos momentos ',
   },
   {
     id: 3,
     title: 'La rompiste 💅',
     imgUrl: 'https://media.tenor.com/Ci-drwiLcqoAAAAC/diva-shades.gif',
     details:
-      'Tu buena predisposición y compromiso con tu pareja te retribuirá como el/la dios/a que eres',
+      'Tu buena predisposición y compromiso con tu pareja te retribuirá como buen novio/a que eres',
   },
 ];
